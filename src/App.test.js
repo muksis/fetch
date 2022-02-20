f766eeaf42ec72408da6656275ee6b0cc503392a
muksis <60219890+muksis@users.noreply.github.com>
2022-02-20 15:44:24 -0500

    Improve test

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom';
-import {getQueriesForElement} from '@testing-library/dom';
+import {render} from '@testing-library/react';
 import App from './App';
 
 test('CatPic', () => {
-  const root = document.createElement('div');
-  ReactDOM.render(<App />, root);
-  const {getByText} = getQueriesForElement(root);
-
-  // after rendering our component
-  // use DOM APIs (query selector) to make assertions
-  expect(root.querySelector('h1').textContent).toBe('Just a random cat pic (=•́ܫ•̀=)');
+  const {getByText} = render(<App />);
   expect(getByText('Just a random cat pic (=•́ܫ•̀=)')).not.toBeNull();
 });
