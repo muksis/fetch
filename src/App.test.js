ce3cb8dcee2b4972698f4189cfa56d5531aa41a1
muksis <60219890+muksis@users.noreply.github.com>
2022-02-20 15:37:10 -0500

    Add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import {getQueriesForElement} from '@testing-library/dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('CatPic', () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<App />, root);
+  const {getByText} = getQueriesForElement(root);
+
+  // after rendering our component
+  // use DOM APIs (query selector) to make assertions
+  expect(root.querySelector('h1').textContent).toBe('Just a random cat pic (=•́ܫ•̀=)');
+  expect(getByText('Just a random cat pic (=•́ܫ•̀=)')).not.toBeNull();
 });
